1d2d0f31ab68084999ac58aacb59fc856aa8bfa8
Tim <timjogorman@gmail.com>
2019-10-01 22:23:32 -0400

    correcting tests, still not perfectly modeling wordpieces or outputing the variables correctly

--- tests_for_transformers.py
+++ tests_for_transformers.py
@@ -11,7 +11,11 @@ with open(amrfilename) as amr_file:
         raw_amr = "\n".join([line for line in each_amr.strip().split("\n") if not line.startswith("#")])
         if raw_amr.strip() != '':
             its_amr = penman.decode(raw_amr, reify_attributes=True)
-            its_dict = its_amr.amr_transformer_path(max_distance=5, shortening_method="end", plusminus=True, ordered_variable_list=["o", "c", "d", "x102", "x103"])
+            its_dict = its_amr.amr_transformer_path(max_distance=5, shortening_method="end", plusminus=True, ordered_variable_list=test_unit['variable-order'])
             its_valid = its_dict['path'] == test_unit['relation']
             if its_valid:
                 print("Pass test for type:  "+test_unit["testname"])
+            else:
+                print("Fail test for type:  "+test_unit["testname"])
+                print(its_dict['path'])
+                print(test_unit['relation'])                
